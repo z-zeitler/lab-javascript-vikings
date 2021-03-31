5ae34dff7ab1143481866f01fa2ea8d36390666e
Zoe Zeitler <zlz97@hotmail.com>
2021-03-31 14:16:26 +0200

    31 March 14:16

--- src/viking.js
+++ src/viking.js
@@ -1,8 +1,25 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        function attack() {
+            return strength;
+        }
+
+        function receiveDamage(damage) {
+            health -= damage;
+        }
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+
+        function battleCry() {
+            return "Odin Owns You All!";
+        }
+    }
+}
 
 // Saxon
 class Saxon {}
