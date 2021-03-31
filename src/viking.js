62337c576ee62f4918daa114a90a89056c470a0b
Zoe Zeitler <zlz97@hotmail.com>
2021-03-31 16:42:01 +0200

    31 March 16:41

--- src/viking.js
+++ src/viking.js
@@ -1,28 +1,68 @@
 // Soldier
 class Soldier {
     constructor(health, strength) {
-        function attack() {
-            return strength;
-        }
+        this.health = health;
+        this.strength = strength;
+    }
 
-        function receiveDamage(damage) {
-            health -= damage;
-        }
+    attack() {
+        return this.strength;
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
     }
 }
+
 
 // Viking
 class Viking extends Soldier {
     constructor(name, health, strength) {
+        super(health, strength);
+        this.name = name;
+    }
 
-        function battleCry() {
-            return "Odin Owns You All!";
+    receiveDamage(damage) {
+        this.health -= damage;
+
+        if(this.health > 0) {
+            return `${this.name} has received ${damage} points of damage`;
+        }
+
+        if(this.health <= 0) {
+            return `${this.name} has died in act of combat`;
+        }
+    }
+
+    battleCry() {
+        return "Odin Owns You All!";
+    }
+    
+}
+
+
+// Saxon
+class Saxon extends Soldier {
+    constructor(health, strength) {
+        super(health, strength);
+    }
+
+    receiveDamage(damage) {
+        this.health -= damage;
+
+        if(this.health > 0) {
+            return `A Saxon has received ${damage} points of damage`;
+        }
+
+        if(this.health <= 0) {
+            return `A Saxon has died in combat`;
         }
     }
 }
 
-// Saxon
-class Saxon {}
 
+/*
 // War
 class War {}
+
+*/
